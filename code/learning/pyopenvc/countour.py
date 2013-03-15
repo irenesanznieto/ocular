eb2e3192ec68277017095828d1a6d5cd764d79db
Irene <irenesanznieto@gmail.com>
2013-03-15 15:06:23 +0100

    change in the minutes of meeting -enumeration added & indentation to sections

--- code/learning/pyopenvc/countour.py
+++ code/learning/pyopenvc/countour.py
@@ -7,12 +7,12 @@
 import numpy as np
 import cv2
  
-im = cv2.imread('test.jpg')	#read image
+im= cv2.imread('test.jpg')	#read image
+
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)	#gray image
 
 ret,thresh = cv2.threshold(imgray,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
 
 cv2.drawContours(im,contours,-1,(0,255,0),3) #draw contour
 
@@ -24,7 +24,6 @@ area = cv2.contourArea(cnt)
 print "The perimeter is (px):", perimeter, "The area is (px):",area
 
 cv2.imshow('CONTOURS',im)
-
 
 
 cv2.waitKey(0)                           ## Wait for keystroke
