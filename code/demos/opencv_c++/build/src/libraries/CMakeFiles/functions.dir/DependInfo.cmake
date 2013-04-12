c15e9aa73551678558118258836132cb32d644ca
Irene <irenesanznieto@gmail.com>
2013-04-12 18:12:18 +0200

    added first part of the demo c++ opencv && reorganized directories

---
+++ code/demos/opencv_c++/build/src/libraries/CMakeFiles/functions.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peko/tfg_git/code/demos/opencv_c++/src/libraries/functions.cpp" "/home/peko/tfg_git/code/demos/opencv_c++/build/src/libraries/CMakeFiles/functions.dir/functions.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
