fde73a49c36297b6790fb43ef57d5da79f6316ff
Irene <irenesanznieto@gmail.com>
2013-09-24 14:29:42 +0200

    more tutorials adapted to my current pcl version

---
+++ code/learning/PCL/tutorials/BASICS/cloud_viewer/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+
+int
+main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Fill in the cloud data
+  pcl::PCDReader reader;
+  // Replace the path below with the path where you saved your file
+  reader.read<pcl::PointXYZ> ("../table_scene_lms400.pcd", *cloud); //reading from bin directory
+
+	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+  	 viewer.showCloud(cloud);
+   	while (!viewer.wasStopped())
+   	{
+   	}
+
+  return (0);
+}
