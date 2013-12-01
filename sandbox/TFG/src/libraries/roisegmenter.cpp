fbfa2ca17521403a930204349c104b72e9634312
Irene <irenesanznieto@gmail.com>
2013-12-01 19:19:12 +0100

    trying to make roisegmenter work. It compiles, but it does not publish anything -->check

--- sandbox/TFG/src/libraries/roisegmenter.cpp
+++ sandbox/TFG/src/libraries/roisegmenter.cpp
@@ -1,26 +1,71 @@
 #include "roisegmenter.h"
 
-RoiSegmenter::RoiSegmenter()
+RoiSegmenter::RoiSegmenter(): it(nh)
 {
-    point_cloud_sub= nh.subscribe <sensor_msgs::PointCloud2> ("/camera/depth_registered/points", 1, &RoiSegmenter::distance2px, this);
+    //change this afterwards to /TFG/Handloc or something like that
+    point_cloud_sub= nh.subscribe <sensor_msgs::PointCloud2> ("/camera/depth_registered/points", 1, &RoiSegmenter::segment3D, this);
+    point_cloud_pub=nh.advertise <sensor_msgs::PointCloud2> ("/TFG/ROI3D", 1);
+
     coord_pub= nh.advertise <std_msgs::Int32MultiArray> ("/TFG/coord", 1);
 
 
+//    image_sub=it.subscribe("/camera/rgb/image_color", 1, &RoiSegmenter::segment2D, this);
+//    image_pub=it.advertise("/TFG/ROI2D",1);
+
 }
 
-
-void RoiSegmenter:: distance2px(const sensor_msgs::PointCloud2ConstPtr & msg)
+void RoiSegmenter:: segment3D (const sensor_msgs::PointCloud2ConstPtr & cloud)
 {
-    pcl::PointCloud<pcl::PointXYZRGB >::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::fromROSMsg (*msg, *cloud);
+      sensor_msgs::PointCloud2 cloud_filtered;
 
+      pcl::PassThrough<sensor_msgs::PointCloud2> pass;
+      pass.setInputCloud(cloud);
+      pass.setFilterFieldName("z");
+      pass.setFilterLimits(0.0,1.0); // unit : meter
+      pass.filter(cloud_filtered);
+
+      pcl::PointCloud <pcl::PointXYZRGB> cloud_distance;
+      pcl::fromROSMsg(cloud_filtered, cloud_distance);
+      RoiSegmenter::distance2px(cloud_distance);
+
+     //publish ROI 3D
+    point_cloud_pub.publish(cloud_filtered);
+}
+
+void RoiSegmenter:: segment2D (const sensor_msgs::ImageConstPtr & msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv::Point first=cv::Point(coord.data.at(0),coord.data.at(1));
+    cv::Point second=cv::Point(coord.data.at(1),coord.data.at(2));
+
+    cv::Rect myROI(first.x, first.y, second.x, second.y);
+    cv::Mat croppedImage = cv_ptr->image(myROI);
+
+    imshow("CROPPED IMAGE", croppedImage);
+    cv::waitKey(3);
+
+    croppedImage.copyTo(cv_ptr->image);
+    image_pub.publish(cv_ptr->toImageMsg());
+}
+
+void RoiSegmenter:: distance2px( pcl::PointCloud <pcl::PointXYZRGB> cloud)
+{
     pcl::PointXYZRGB totmax, totmin, max, min;
-    pcl::getMinMax3D(*cloud, totmin, totmax);
+    pcl::getMinMax3D(cloud, totmin, totmax);
 
 //	ROS_INFO("Image limits:\n x : %f %f \n y : %f %f\n", totmax.x, totmin.x, totmax.y, totmin.y);
 //	ROS_INFO("ROI limits:\n x : %f %f \n y : %f %f\n", max.x, min.x, max.y, min.y);
 
-    std_msgs::Int32MultiArray coord;
     coord.data.clear();
 
     float distx, disty;
--- sandbox/TFG/src/libraries/roisegmenter.h
+++ sandbox/TFG/src/libraries/roisegmenter.h
@@ -4,14 +4,23 @@
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 
-
 //PCL's includes
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
 
 //Msgs
 #include <std_msgs/Int32MultiArray.h>
 #include <sensor_msgs/PointCloud2.h>
+
+//Image
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 class RoiSegmenter
 {
@@ -22,10 +31,27 @@ private:
 
 
     ros::NodeHandle nh;
+
     ros::Subscriber point_cloud_sub;
+    ros::Publisher point_cloud_pub;
+
     ros::Publisher coord_pub;
 
-    void distance2px(const sensor_msgs::PointCloud2ConstPtr &);
+    image_transport::ImageTransport it;
+
+    image_transport::Publisher image_pub;
+    image_transport::Subscriber image_sub;
+
+    void segment3D (const sensor_msgs::PointCloud2ConstPtr &);
+    void segment2D (const sensor_msgs::ImageConstPtr &);
+
+    void distance2px( pcl::PointCloud <pcl::PointXYZRGB>);
+
+    std_msgs::Int32MultiArray coord;
+
+
+//    void roi3D(pcl::PointCloud <pcl::PointXYZRGB> cloud);
+
 
 };
 #endif //ROI_SEGMENTER_H
