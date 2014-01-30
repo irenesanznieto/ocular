7f92389d4ff5df6925d33b872d5e00229de9deed
Irene <irenesanznieto@gmail.com>
2014-01-30 19:57:35 +0100

    new version of the converter library

--- sandbox/TFG/src/libraries/libraries/converter.cpp
+++ sandbox/TFG/src/libraries/libraries/converter.cpp
@@ -2,25 +2,23 @@
 
 
 Converter::Converter()
-{
-    input_sub=nh.subscribe<pi_tracker::Skeleton>("input", 1, &Converter::input_callback, this);
-    output_sub=nh.advertise<TFG::HandLoc>("output", 1);
-}
+{}
 
 
-void Converter::input_callback(const pi_tracker::SkeletonConstPtr &msg)
+TFG::HandLoc Converter::handCoordinatesExtractor(const pi_tracker::Skeleton &msg)
 {
 
    TFG::HandLoc hand_location;
 
-   hand_location.header=msg->header;
-   hand_location.user_id=msg->user_id;
-   //left_hand --> 5, right_hand --> 8
-   hand_location.name.push_back(msg->name[5]);
-   hand_location.name.push_back(msg->name[8]);
-   hand_location.position.push_back(msg->position[5]);
-   hand_location.position.push_back(msg->position[8]);
+   hand_location.header=msg.header;
+   hand_location.user_id=msg.user_id;
 
-   output_sub.publish(hand_location);
+   //left_hand --> 5, right_hand --> 8
+   hand_location.name.push_back(msg.name[5]);
+   hand_location.name.push_back(msg.name[8]);
+   hand_location.position.push_back(msg.position[5]);
+   hand_location.position.push_back(msg.position[8]);
+
+   return hand_location;
 }
 
--- sandbox/TFG/src/libraries/libraries/converter.h
+++ sandbox/TFG/src/libraries/libraries/converter.h
@@ -2,21 +2,18 @@
 #define CONVERTER_H
 #include <ros/ros.h>
 #include <pi_tracker/Skeleton.h>
-#include <TFG/HandLoc.h>
+#include "../msg_gen/cpp/include/TFG/HandLoc.h"
 #include <std_msgs/String.h>
 
 class Converter 
 {
 public:
     Converter();
+    TFG::HandLoc handCoordinatesExtractor(const pi_tracker::Skeleton &);
+
 
 private:
 
-    ros::NodeHandle nh;
-    ros::Subscriber input_sub;
-    ros::Publisher output_sub;
-
-    void input_callback(const pi_tracker::SkeletonConstPtr &);
 
 };
 #endif // CONVERTER_H
