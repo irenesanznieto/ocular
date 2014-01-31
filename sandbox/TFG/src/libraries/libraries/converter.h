daa1652c9653a1227ec738d3c6bb8cccb050620f
Irene Sanz <irenesanznieto@gmail.com>
2014-01-31 16:09:38 +0100

    converter.h doxygenized

--- sandbox/TFG/src/libraries/libraries/converter.h
+++ sandbox/TFG/src/libraries/libraries/converter.h
@@ -5,16 +5,36 @@
 #include <TFG/HandLoc.h>
 #include <std_msgs/String.h>
 
+//* Converter
+/**
+* This class is used as a bridge between the software used to locate the hand's position (in this case, the ROS package pi_tracker) and the project. 
+* From the pi_tracker::Skeleton message, it fills the TFG::HandLoc message. 
+*   
+*/
+
 class Converter 
 {
+    
 public:
+
+    /*!
+    * \brief Public constructor
+    */
     Converter();
-    TFG::HandLoc handCoordinatesExtractor(const pi_tracker::Skeleton &);
+    
+    /*!
+    * \brief Method that extracts the hands information from the pi_tracker::Skeleton message
+    *
+    * This description is displayed lower in the doxygen as an extended description along with
+    * the above brief description.
+    * \param msg       The pi_tracker::Skeleton message provided by the ROS package pi_tracker
+    */
+    TFG::HandLoc handCoordinatesExtractor(const pi_tracker::Skeleton & msg);
 
 
 private:
 
+    TFG::HandLoc hand_location;    /** @todo Is this variable really used?? */
 
-    TFG::HandLoc hand_location;
 };
 #endif // CONVERTER_H
