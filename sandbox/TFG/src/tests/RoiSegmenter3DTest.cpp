b1cc20904fef77a0e39361b618cfb03a13095059
Irene <irenesanznieto@gmail.com>
2014-02-05 16:25:13 +0100

    roisegmenter3Dtest skeleton compiling

--- sandbox/TFG/src/tests/RoiSegmenter3DTest.cpp
+++ sandbox/TFG/src/tests/RoiSegmenter3DTest.cpp
@@ -2,41 +2,41 @@
 #include <gtest/gtest.h>
 
 
-//class RoiSegmenter3DTest : public testing::Test
-//{
+class RoiSegmenter3DTest : public testing::Test
+{
 
-//public:
-////    RoiSegmenter3D roiseg3D;
-////    TFG::HandLoc example;
+public:
+    RoiSegmenter3D roiseg3D;
+//    TFG::HandLoc example;
 
-//    virtual void SetUp()
-//    {
-//        //    example.header=;
-//        //    example.user_id=;
-//        //    example.name=;
-//        //    example.position=;
-//    }
+    virtual void SetUp()
+    {
+        //    example.header=;
+        //    example.user_id=;
+        //    example.name=;
+        //    example.position=;
+    }
 
-//    virtual void TearDown()
-//    {
-//    }
-//};
+    virtual void TearDown()
+    {
+    }
+};
 
-//TEST_F(RoiSegmenter3DTest,segment)
-//{
+TEST_F(RoiSegmenter3DTest,segment)
+{
 
-//}
+}
 
 
-//TEST_F(RoiSegmenter3D,distance2px)
-//{
-////    roiseg3D.setHandLoc(example);
-//}
+TEST_F(RoiSegmenter3DTest,distance2px)
+{
+//    roiseg3D.setHandLoc(example);
+}
 
-//TEST_F(RoiSegmenter3D,coordinates)
-//{
-////    roiseg3D.coordinates(example);
-//}
+TEST_F(RoiSegmenter3DTest,coordinates)
+{
+//    roiseg3D.coordinates(example);
+}
 
 
 int main (int argc, char **argv)
