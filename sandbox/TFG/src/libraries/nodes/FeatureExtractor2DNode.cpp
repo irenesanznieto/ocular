320d7c801bb6008b535bad160525c7f7bad38391
Irene <irenesanznieto@gmail.com>
2014-02-13 17:07:26 +0100

    corrected little mistake -now everything compiling-

--- sandbox/TFG/src/libraries/nodes/FeatureExtractor2DNode.cpp
+++ sandbox/TFG/src/libraries/nodes/FeatureExtractor2DNode.cpp
@@ -2,7 +2,7 @@
 
 FeatureExtractor2DNode::FeatureExtractor2DNode()/*: it(nh)*/
 {
-    image_sub=/*it.*/nh.subscribe<TFG::HandImage>("input_image", 1, &FeatureExtractor2D::input_image_cb, this);
+    image_sub=/*it.*/nh.subscribe<TFG::HandImage>("input_image", 1, &FeatureExtractor2DNode::input_image_cb, this);
 }
 
 void FeatureExtractor2DNode::input_image_cb (const TFG::HandImageConstPtr &msg)
