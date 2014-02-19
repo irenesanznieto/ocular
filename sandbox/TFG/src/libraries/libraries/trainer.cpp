708d56d40d534cf68fc2a144ef617fe98a45e4c7
Irene <irenesanznieto@gmail.com>
2014-02-19 17:18:14 +0100

    working on trainer

--- sandbox/TFG/src/libraries/libraries/trainer.cpp
+++ sandbox/TFG/src/libraries/libraries/trainer.cpp
@@ -2,28 +2,32 @@
 
 Trainer::Trainer()
 {
-
     //TODO: add the descriptors reading from the files!!
 
     //Add the descriptors to the algorithm
-    alg2D.add(descriptors);
-    alg2D.train();
+
+    for (unsigned int i =0; i<descriptors.size(); i++)
+    {
+        alg2D[i].add(descriptors[i]);
+        alg2D[i].train();
+    }
 }
 
-void Trainer::train(const sensor_msgs::PointCloud2ConstPtr & pc_msg, const sensor_msgs::ImageConstPtr & image_msg)
+
+//REMEMBER TO CHANGE SET_NEW_OBJECT TO TRUE WHENEVER WE ARE LEARNING A NEW OBJECT!!!!!
+void Trainer::train(const sensor_msgs::ImageConstPtr & descriptors)
 {
     //convert from ros image msg to opencv image
     cv_bridge::CvImagePtr cv_ptr;
 
     try
     {
-        cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+        cv_ptr = cv_bridge::toCvCopy(descriptors, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception& e)
     {
         ROS_ERROR("cv_bridge exception: %s", e.what());
     }
-
 
     //train 2D features
     this->train2D(cv_ptr->image);
@@ -33,6 +37,8 @@ void Trainer::train(const sensor_msgs::PointCloud2ConstPtr & pc_msg, const senso
 
     //store algorithm
     this->save_algorithm();
+
+
 
 
     //train 3D features
@@ -46,18 +52,73 @@ void Trainer::train(const sensor_msgs::PointCloud2ConstPtr & pc_msg, const senso
 
 void Trainer::train2D(cv::Mat image_cv)
 {
-    descriptors.push_back(image_cv);
-    alg2D.train();
+    //If it is a new object, we want to create a new object position in the vector
+    int object_number=descriptors.size();
+
+    //If it is not a new object, we want to add the descriptors to the last object:
+    if (new_object!=true)
+        object_number-=1;
+
+
+    descriptors[object_number].push_back(image_cv);
+    alg2D[object_number].train();
 }
 
 
 void Trainer::save_template()
 {
+    //If it is a new object, we want to create a new object position in the vector
+    int object_number=descriptors.size();
 
+    //If it is not a new object, we want to add the descriptors to the last object:
+    if (new_object!=true)
+        object_number-=1;
+
+    //NAME CODE:
+    std::stringstream filename;
+    filename <<"../data/templates/"<<object_number<<"/";
+
+    for (unsigned int i=0; i<descriptors.size(); i++)
+    {
+        filename<<"view_"<< i<<"_d.yml";
+            cv::FileStorage fs(filename.str(), cv::FileStorage::WRITE);
+            fs <<"descriptors"<< descriptors[object_number][i];
+
+    }
 }
 
 void Trainer::save_algorithm()
 {
+
+}
+
+
+void Trainer ::set_new_object(bool new_object)
+{
+    this->new_object=new_object;
+}
+
+
+std::vector <std::string> Trainer:: getTemplates (std::string path)
+{
+        std::string sys_command="ls "+path;
+        std::system(sys_command.c_str());
+
+        std::ifstream file(path.c_str());
+        std::string dummy;
+        std::vector <std::string> templates;
+
+        while (file.good())
+        {
+            getline(file,dummy);
+            templates.push_back(dummy);
+        }
+
+        file.close();
+
+        templates.erase(templates.end()); //Not sure where this vector creates an empty space in the last row. Anyway, not throwing errors this way neither losing info.
+
+        return templates;
 
 }
 
--- sandbox/TFG/src/libraries/libraries/trainer.h
+++ sandbox/TFG/src/libraries/libraries/trainer.h
@@ -7,25 +7,34 @@
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <fstream>
 
 class Trainer
 {
 public:
 
     Trainer();
-    void train(const sensor_msgs::PointCloud2ConstPtr & , const sensor_msgs::ImageConstPtr & );
+    void train(const sensor_msgs::ImageConstPtr & descriptors);
+
+    void set_new_object(bool new_object);
+
+    std::vector <std::string> getTemplates (std::string path);
+
 
 private:
 
     void train2D(cv::Mat);
 
-    cv::FlannBasedMatcher alg2D;
+    std::vector<cv::FlannBasedMatcher >alg2D;
 
-    std::vector<cv::Mat> descriptors;    /** Vector that will store the descriptors dataset */
+    std::vector<std::vector<cv::Mat> >descriptors;    /** Vector that will store the descriptors dataset */
 
 
     void save_template();
     void save_algorithm();
+
+
+    bool new_object;
 
 };
 
