9a1530d1da3639f1d04c7a63b6e0a9cd349341be
Irene <irenesanznieto@gmail.com>
2014-03-04 16:34:51 +0100

    EventHandlerNode finished -todo: check constants

--- sandbox/TFG/src/apps/event_handler.cpp
+++ sandbox/TFG/src/apps/event_handler.cpp
@@ -5,7 +5,7 @@
 int main (int argc,char* argv[])
 {
     ros::init(argc, argv, "CONVERTER");
-    EventHandler eventhandler;
+    EventHandlerNode eventhandler;
 
     ros::spin();
 
--- sandbox/TFG/src/libraries/nodes/EventHandlerNode.h
+++ sandbox/TFG/src/libraries/nodes/EventHandlerNode.h
@@ -2,6 +2,10 @@
 #define EVENTHANDLERNODE_H
 #include "../libraries/eventhandler.h"
 #include <ros/ros.h>
+#include <pi_tracker/Skeleton.h>
+#include <TFG/EventHandler.h>
+#include <pcl/point_types.h>
+
 
 class EventHandlerNode
 {
@@ -10,11 +14,21 @@ public:
     ros::NodeHandle nh;
     ros::Publisher event_pub;
 
-//    subscribe to the skeleton message:
-//    ros::Subscriber
+    ros::Subscriber skeleton_sub;      /**Subscriber used to obtain the pi_tracker::Skeleton messages*/
+
 
 private:
     EventHandler eventHandler;
+    /*!
+        * \brief Callback method for the subscriber topic.
+        *
+        *\param pi_tracker::SkeletonConstPtr message received through the topic
+        *
+        * Compares the position of the hands to determine which one is being used in the program (the other one will be located at the body's side).
+        * Also, decides which mode (learner or recognizer) is active depending on the hand's position. If it is extended towards the RGB-D sensor,
+        *the learner mode will be activated. Otherwise, the recognizer mode will be activated.
+        */
+    void event_cb(const pi_tracker::SkeletonConstPtr &);
 
 };
 
