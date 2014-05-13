a096bff923b65c24021bedd76a784223602b7baf
irenesanznieto <irenesanznieto@gmail.com>
2014-05-13 19:52:07 +0200

    python script write to file updated -fully working now-

--- src/apps/write_to_file.py
+++ src/apps/write_to_file.py
@@ -7,41 +7,43 @@ import os
 import fnmatch
 import sys
 
-#def callback (data):
+from ocular.msg import RecognizedObject
+
+
+def callback (data):
+
+    f=file(path_file, 'a')
+
+    for i in range(len(data.object_id)):
+        sum=str(data.object_id[i])+' '+str(data.grade_certainty[i])+'\n'
+        f.write(sum)
+
 #    rospy.loginfo(rospy.get_caller_id()+"I heard %s", data.data)
 
 
-#def listener():
+def listener():
 
-#        rospy.init_node('listener',anonymous=True)
-#        r=rospy.Rate(10)
+        rospy.init_node('writer2file',anonymous=True)
+        r=rospy.Rate(10)
 
-#        rospy.Subscriber("/TFG/whatever", String, callback)
-#        rospy.spin()
+        rospy.Subscriber("object_id", Recognized_Object, callback)
+        rospy.spin()
 
-
-
-if __name__=='__main__':
-
-#        call(['ls'])
-#        matches=[]
-#        for root, dirnames, filenames in os.walk('src'):
-#            for filename in fnmatch.filter(filenames, 'output.txt'):
-#                matches.append(os.path.join(root, filename))
+def pathfinder():
 
     for  root, dir, files in os.walk('.'):                      #retrieve all files and folders in the current folder
-        for d in dir:
-#            print d
-            if d == 'ocular':                                   #if one of them is called ocular
-                d_abs=os.path.join(root,d)                      #store the absolute path to the folder in d_abs
-#                print d_abs
+            for d in dir:
+    #            print d
+                if d == 'ocular':                                   #if one of them is called ocular
+                    d_abs=os.path.join(root,d)                      #store the absolute path to the folder in d_abs
+    #                print d_abs
 
-                for root_o, dir_o, files_o in os.walk(d_abs):   #retrieve all files inside the path to the oculus folder
-                    for d_o in dir_o:                           #search for a folder within
-#                        print d_o
-                        if d_o=='data':                         #if the folder is called data
-                            absolute_path=os.path.join(d_abs,d_o)       #print the absolute path to that folder!
-#                            print absolute_path
+                    for root_o, dir_o, files_o in os.walk(d_abs):   #retrieve all files inside the path to the oculus folder
+                        for d_o in dir_o:                           #search for a folder within
+    #                        print d_o
+                            if d_o=='data':                         #if the folder is called data
+                                absolute_path=os.path.join(d_abs,d_o)       #print the absolute path to that folder!
+    #                            print absolute_path
 
     absolute_path+='/'
     if len(sys.argv)==2:
@@ -54,7 +56,24 @@ if __name__=='__main__':
         else:
             print 'usage: exec name_file or exec'
 
+    return path_file
 
-    f=file(path_file, 'w')
-    f.write('jelou again!')
-#        listener()
+
+path_file=pathfinder()
+
+if __name__=='__main__':
+
+    print path_file
+
+    data=RecognizedObject()
+
+    data.object_id[0]=20
+    data.object_id[1]=30
+
+
+    data.grade_certainty[0]=0.3
+    data.grade_certainty[1]=0.8
+
+    callback(data)
+
+#    listener()
