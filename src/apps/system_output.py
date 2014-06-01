88ce1356f38023a6d146f545255ea8b9dffde067
Irene <irenesanznieto@gmail.com>
2014-06-01 13:34:29 +0200

    modified system output, added id2string that will store conversion from id to the object name

--- src/apps/system_output.py
+++ src/apps/system_output.py
@@ -5,11 +5,67 @@ from collections import Counter
 import operator
 from ocular.msg import RecognizedObject
 from std_msgs.msg import Int16
+import numpy as np
 
 
 def frequency_counter(a):
     c=Counter(a)
     return c.values(),  c.keys()
+
+def decision(v2d, k2d, v3d, k3d):
+
+    if k2d[len(k2d)-1] >= k3d[len(k3d)-1]:
+        max_object_id = k2d[len(k2d)-1]
+
+    else:
+        max_object_id = k3d[len(k3d)-1]
+
+    #print max_object_id
+
+#    while len(k2d)<max_object_id:
+#        k2d.append(0)
+
+#    while len(k3d)<max_object_id:
+#        k3d.append(0)
+
+	for i in range(0, max_object_id+1):
+		#print (k2d, k3d,i)
+		#print (len(k2d), len(k3d), i) 
+		
+		if (len(k2d)-1)<i:
+			k2d.append(i)
+			v2d.append(0)
+			
+		else:
+			if k2d[i] != i:
+				k2d.insert(i, i)
+				v2d.insert(i, 0)
+
+		if (len(k3d)-1)<i:
+			k3d.append(i)
+			v3d.append(0)
+			
+		else:
+			if k3d[i] != i:
+				k3d.insert(i, i)
+				v3d.insert(i, 0)
+
+        #print (k2d[i], k3d[i],i,"\n")
+
+    #print(k2d, k3d)#, v2d , v3d)
+	
+	if len(v2d) == len(v3d):
+		values=np.multiply(0.6,v2d)+np.multiply(0.4,v3d)
+		keys=k2d
+		max_value = (max(xrange(len(values)), key=values.__getitem__))
+		print (v2d, v3d, values)
+		print ('Final decision: ', keys[max_value])
+		return keys[max_value]
+		
+	else: 
+		print ('lenght vectors not matching: ', v2d, v3d)
+
+
 
 def callback(data):
     iterations=30
@@ -29,11 +85,14 @@ def callback(data):
 
     if callback.counter<=iterations:
     	#add the new data to the lists
-        callback.f2d.append(data.object_id[0]);
-        callback.f3d.append(data.object_id[1]);
+    	if data.object_id[0] != -1 : 
+        	callback.f2d.append(data.object_id[0]);
+        
+		if data.object_id[1] != -1 : 
+			callback.f3d.append(data.object_id[1]);
 
 
-        if callback.counter == iterations:
+        if callback.counter == iterations and len(callback.f3d) >0 and len (callback.f2d)>0:
         	#obtain the frequency of each element in the lists
             v2d, k2d=frequency_counter(callback.f2d)
             v3d, k3d=frequency_counter(callback.f3d)
@@ -43,15 +102,13 @@ def callback(data):
             max3d = (max(xrange(len(v3d)),key=v3d.__getitem__))
             
             #print object_id 2D and 3D 
-            print('2D  Max frequency: ', k2d[max2d]) #,v2d ,k2d)
-            print('3D  Max frequency: ', k3d[max3d]) #, v3d ,k3d)
+            #print('2D ', k2d[max2d]) #,v2d ,k2d)
+            #print('3D ', k3d[max3d]) #, v3d ,k3d)
+            
+            #decide final object & publish it
+            pub.publish(decision(v2d, k2d, v3d, k3d))
 
-			#decide final object & publish it
-			#values=0.6*v2d + 0.4*v3d
-			#max_index = (max(xrange(len(values)),key=values.__getitem__))
-            #pub.publish(keys[max_it])
-
-
+			
 			#clear lists and counter
             callback.f2d = []
             callback.f3d = []
@@ -60,7 +117,7 @@ def callback(data):
 
 
 if __name__ == "__main__":
-    pub = rospy.Publisher('output',Int16)
+    pub = rospy.Publisher('final_object_id',Int16)
     rospy.init_node('system_output',anonymous=True)
     r=rospy.Rate(10)
     rospy.Subscriber("object_id", RecognizedObject, callback)
