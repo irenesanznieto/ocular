8c66912d6c337e17d8b00ff3779f1266824e4a4b
Irene <irenesanznieto@gmail.com>
2014-06-01 19:52:06 +0200

    updated id2string

--- src/apps/id2string.py
+++ src/apps/id2string.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from ocular.msg import RecognizedObject
+from std_msgs.msg import Int16
 
-def callback(data):
+def callback (data):
+
 	objects=['pelota','taza']
-	if data >=0:
-		pub.publish(objects[data])
+	if data.data >=0 and len(objects) > data.data :
+		print objects[data.data]
+		pub.publish(objects[data.data])
 	else: 
-		pub.publish('no_object')
+		pub.publish('NO_OBJECT_RECOGNIZED')
 
 
 if __name__ == "__main__":
     pub = rospy.Publisher('final_object_name',String)
     rospy.init_node('id2string',anonymous=True)
     r=rospy.Rate(10)
-    rospy.Subscriber("final_object_id", RecognizedObject, callback)
+    rospy.Subscriber("final_object_id", Int16, callback)
     rospy.spin()
